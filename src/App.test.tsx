86d24f614409684a1941211115b4dd460f676626
George Kemp <georgekemp94@gmail.com>
2018-03-30 11:14:15 +0100

    Adds Rubric as Empty Tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -6,3 +6,43 @@ it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
 });
+
+describe('Main Page',() => {
+  it('Shows three categories for books', ()=> {
+    
+  })
+
+  it('Allows users to move books between shelves',()=>{
+
+  })
+
+  it('Persists Information between browser refreshes',()=>{
+
+  })
+});
+
+describe('Search Page',() => {
+  it('Has search input that lets users search for books', ()=> {
+    
+  })
+
+  it('Search results allow a user to categorize a book',()=>{
+
+  })
+
+  it('Selections made on the search page show up on the main page',()=>{
+
+  })
+});
+
+describe('Routing',() => {
+  it('The main page links to the search page', ()=> {
+    
+  })
+
+  it('The search page links to the main page',()=>{
+
+  })
+});
+
+
